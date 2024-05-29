d2acb2f1750ddae4cde1f71f0d6304057d980ac3
pravallikavanjarapu <pravallikaradha57@gmail.com>
2024-05-29 16:01:47 +0530

    first commit

---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get install build-essential cmake pkg-config libx11-dev libatlas-base-dev libgtk-3-dev libboost-python-dev
+python3 -m venv projectenv
+source projectenv/bin/activate
+pip3 install -r requirements.txt
